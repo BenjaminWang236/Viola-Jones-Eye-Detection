5dd03ab4694d726bf7790a5b17c37deccc0e78b1
BenjaminWang236 <44148168+BenjaminWang236@users.noreply.github.com>
2019-07-31 17:32:40 -0700

    cython

---
+++ compile.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(
+    ext_modules = cythonize("ViolaJones.py", compiler_directives={'language_level' : "3"})
+)
+
+
+# from distutils.core import setup
+# from distutils.extension import Extension
+# from Cython.Distutils import build_ext
+# ext_modules = [
+#     Extension("mymodule1",  ["mymodule1.py"]),
+#     Extension("mymodule2",  ["mymodule2.py"]),
+# #   ... all your modules that need be compiled ...
+# ]
+# setup(
+#     name = 'Viola_Jones_Eye_Detection',
+#     cmdclass = {'build_ext': build_ext},
+#     ext_modules = ext_modules
+# )
