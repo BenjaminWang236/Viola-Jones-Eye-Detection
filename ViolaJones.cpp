3bf99db78f4a3afedc289c03a1cb7b6321002f02
BenjaminWang236 <44148168+BenjaminWang236@users.noreply.github.com>
2019-09-03 10:27:01 -0700

    cpping

--- ViolaJones.cpp
+++ ViolaJones.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+#include <array>
+
+#include <bmp.h>
+
 using namespace std;
+
+/*Global variables*/
+vector<char> buffer;
+PBITMAPFILEHEADER file_header;
+PBITMAPINFOHEADER info_header;
+
+/*Loading the BMP images into vector*/
+
+void fill(string &fname) {
+    std::ifstream file(fname);
+
+    if (file) {
+        file.seekg(0,std::ios::end);
+        streampos length = file.tellg();
+        file.seekg(0,std::ios::beg);
+
+        buffer.resize(length);
+        file.read(&buffer[0],length);
+
+        file_header = (PBITMAPFILEHEADER)(&buffer[0]);
+        info_header = (PBITMAPINFOHEADER)(&buffer[0] + sizeof(BITMAPFILEHEADER));
+    }
+}
+
 int main() 
 {
-    cout << "Hello, World!";
+    string fname = "data/database0/training_set/training1.bmp";
+    fill(fname);
+    cout << buffer[0] << buffer[1] << endl;
+    cout << file_header->bfSize << endl;
+    cout << info_header->biWidth << " " << info_header->biHeight << endl;
+    
     return 0;
 }
