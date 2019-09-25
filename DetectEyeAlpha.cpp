082cbb8bb5e08e362153e190431404a5b705a004
BenjaminWang236 <44148168+BenjaminWang236@users.noreply.github.com>
2019-09-25 10:00:47 -0700

    updating

--- DetectEyeAlpha.cpp
+++ DetectEyeAlpha.cpp
@@ -7,7 +7,7 @@
 #include <cmath>
 #include <algorithm>    
 #include <chrono>
-#include "timer.h"
+// #include "timer.h"
 //#include <array> 
 //#include <iomanip>      // std::setw
 
@@ -27,6 +27,19 @@ img_0.bmp etc for testing, trainimg_0.bmp etc for training.
 string WorkFolder = "/Users/infin/OneDrive/NeuronBasic/Viola-Jones-Eye-Detection/";
 string ImageFolder = "trainimg/";
 string ImgOutFolder = "detected/";
+
+string format_duration(long dur) {
+	microseconds milsec(dur);
+	auto ms = duration_cast<milliseconds>(milsec);		milsec -= duration_cast<microseconds>(ms);
+	auto secs = duration_cast<seconds>(ms);				ms -= duration_cast<milliseconds>(secs);
+	auto mins = duration_cast<minutes>(secs);			secs -= duration_cast<seconds>(mins);
+	auto hour = duration_cast<hours>(mins);				mins -= duration_cast<minutes>(hour);
+
+	stringstream ss;
+	ss << hour.count() << "h " << mins.count() << "m " << secs.count() << "s " <<
+			ms.count() << "ms " << milsec.count() << "\xE6s";
+	return ss.str();
+}
 
 
 struct TableList {
