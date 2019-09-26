a30f5acca59ca25c35c5590ffe1a314b785d35b6
BenjaminWang236 <44148168+BenjaminWang236@users.noreply.github.com>
2019-09-25 17:19:03 -0700

    hog working!

--- HogDetector.py
+++ HogDetector.py
@@ -1,6 +1,15 @@
 import cv2
 import numpy as np
 from skimage import feature
+from skimage import exposure
 
-hog = cv2.HOGDescriptor()
+# hog = cv2.HOGDescriptor()
 im = cv2.imread('trainimg_0.bmp')
+# h = hog.compute(im)
+
+(H, hogImage) = feature.hog(im, orientations=9, pixels_per_cell=(8, 8),
+    cells_per_block=(2, 2), transform_sqrt=True, block_norm="L1", visualize=True)
+hogImage = exposure.rescale_intensity(hogImage, out_range=(0, 255))
+hogImage = hogImage.astype("uint8")
+cv2.imshow("HOG Image", hogImage)
+cv2.waitKey(0)
